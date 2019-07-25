0f961b9cb958ae3a339ee9c573baa08476c92045
chengcj-upenn <chengcj@seas.upenn.edu>
2019-07-25 20:45:04 +0200

    added hw related files

---
+++ src/edu/upenn/cit594/Main.java
@@ -0,0 +1,13 @@
+package edu.upenn.cit594;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO need to make it run from command line and take args
+		String fileFormat = "csv"; // either csv or json
+		String parkViolateFilename = "parking.csv";
+		String populationInputFilename = "population.txt";
+
+	}
+
+}
