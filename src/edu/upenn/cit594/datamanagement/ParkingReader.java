032177697df14f92bdbb2fe70616b53234c03147
vineelapentyala <pentyala@seas.upenn.edu>
2019-07-27 18:48:55 -0400

    updated ParkingReader and PopulationReader classes

--- src/edu/upenn/cit594/datamanagement/ParkingReader.java
+++ src/edu/upenn/cit594/datamanagement/ParkingReader.java
@@ -1,5 +1,71 @@
 package edu.upenn.cit594.datamanagement;
+import java.io.*;
+import java.util.*;
+
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import edu.upenn.cit594.data.ParkingViolation;
 
 public class ParkingReader {
 
+    private List<ParkingViolation> allParkingViolations;
+    
+    private void storeParkingViolations(int zip, double fine) {
+	ParkingViolation thisViolation = new ParkingViolation(zip, fine);
+	allParkingViolations.add(thisViolation);
+    }
+    
+    public void parseCSV(String inputFileName) {
+	File inputFile = new File(inputFileName);
+	String[] violationLine = new String[7];
+	try {
+	    Scanner s = new Scanner(inputFile);
+	    s.nextLine();
+	    while (s.hasNextLine()) {
+		String line = s.nextLine();
+		violationLine = line.split(",");
+		if (violationLine[6]!= "" && violationLine[4] == "PA") {
+		    int zip = Integer.parseInt(violationLine[6]);
+		    double fine = Integer.parseInt(violationLine[1]);
+		    storeParkingViolations(zip, fine);
+		}
+	    }
+	    s.close();
+	} catch (FileNotFoundException e) {
+	    System.out.println("Incorrect input File name"); //error message to be displayed when input filename cannot be found
+	}
+    }
+    
+    private void parseJSON(String inputFileName) {
+	JSONParser parser = new JSONParser();
+	
+	try {
+	    JSONArray allViolationLines = (JSONArray)parser.parse(new FileReader("inputFileName"));
+	    Iterator iter = allViolationLines.iterator();
+	    while (iter.hasNext()) {
+		JSONObject  violationLine = (JSONObject) iter.next();
+		if (violationLine.get("zip_code")!= "" && violationLine.get("state") == "PA") {
+		    int zip = (int) violationLine.get("zip_code");
+		    double fine = (double)violationLine.get("state");
+		    storeParkingViolations(zip, fine);
+		}
+		
+	    }
+	    
+	} catch (FileNotFoundException e) {
+	    System.out.println("Incorrect input File name");
+	} catch (IOException e) {
+	    System.out.println("IO Exception");
+	} catch (ParseException e) {
+	    System.out.println("ParseException");
+	}
+    }
+
+    public List<ParkingViolation> getAllParkingViolations() {
+        return allParkingViolations;
+    }
+    
+    
 }
--- src/edu/upenn/cit594/datamanagement/PopulationReader.java
+++ src/edu/upenn/cit594/datamanagement/PopulationReader.java
@@ -1,5 +1,37 @@
 package edu.upenn.cit594.datamanagement;
 
-public class PopulationReader {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class PopulationReader {
+    
+   private  HashMap<Integer, Integer> populationMap;
+   
+   
+   public void parseTxt(String inputFileName) {
+	File inputFile = new File(inputFileName);
+	String[] zipAndpopulation = new String[2];
+	try {
+	    Scanner s = new Scanner(inputFile);
+	    s.nextLine();
+	    while (s.hasNextLine()) {
+		String line = s.nextLine();
+		zipAndpopulation = line.split(" ");
+		    int zip = Integer.parseInt(zipAndpopulation[0]);
+		    int population = Integer.parseInt(zipAndpopulation[1]);
+		    populationMap.put(zip, population);
+	    }
+	    s.close();
+	} catch (FileNotFoundException e) {
+	    System.out.println("Incorrect input File name"); //error message to be displayed when input filename cannot be found
+	}
+   }
+
+   public HashMap<Integer, Integer> getPopulationMap() {
+    return populationMap;
+   }
+   
+   
 }
