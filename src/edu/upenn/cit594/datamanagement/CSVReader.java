22eebfcb90f90de167bdf8c466f411bae16f9c60
vineelapentyala <pentyala@seas.upenn.edu>
2019-07-29 22:33:03 -0400

    updated CSVReader

--- src/edu/upenn/cit594/datamanagement/CSVReader.java
+++ src/edu/upenn/cit594/datamanagement/CSVReader.java
@@ -5,35 +5,34 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class CSVReader extends ParkingReader {
-	public CSVReader(String inputFileName) throws Exception {
-		super(inputFileName);
+    public CSVReader(String inputFileName) throws Exception {
+	super(inputFileName);
+    }
+
+    /**
+     * parseCSV reads the parking violations data from a .csv file and stores all
+     * valid parking violations to 'allParkingViolations' List
+     * 
+     * @param inputFileName .csv file with the parking violations information
+     * @throws FileNotFoundException
+     */
+    public void parse() throws FileNotFoundException {
+	File inputFile = new File(inputFileName);
+
+	Scanner s = new Scanner(inputFile);
+	while (s.hasNextLine()) {
+	    String line = s.nextLine();
+	    String[] violationLine = line.split(",");
+	    int zip = 0;
+	    if (violationLine.length == 7) {
+		zip = Integer.parseInt(violationLine[6]);
+	    }
+
+	    double fine = Integer.parseInt(violationLine[1]);
+	    String state = violationLine[4];
+	    storeParkingViolations(zip, fine, state);
 	}
-
-	/**
-	 * parseCSV reads the parking violations data from a .csv file and stores all
-	 * valid parking violations to 'allParkingViolations' List
-	 * 
-	 * @param inputFileName .csv file with the parking violations information
-	 * @throws FileNotFoundException
-	 */
-	public void parse() throws FileNotFoundException {
-		File inputFile = new File(inputFileName);
-
-		Scanner s = new Scanner(inputFile);
-		while (s.hasNextLine()) {
-			String line = s.nextLine();
-			String[] violationLine = line.split(",");
-			if (violationLine.length == 7) {
-				if (violationLine[6] != null && violationLine[4].equals("PA")) {
-					int zip = Integer.parseInt(violationLine[6]);
-					double fine = Integer.parseInt(violationLine[1]);
-					String state = violationLine[4];
-					storeParkingViolations(zip, fine, state);
-				}
-			}
-		}
-		s.close();
-
-	}
+	s.close();
+    }
 
 }
